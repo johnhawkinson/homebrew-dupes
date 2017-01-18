450c7af7891ec5ec7672d2cd0b064c2e040cd04e
Kazuki Suda <kazuki.suda@gmail.com>
2017-01-18 22:08:10 +0900

    screen 4.5.0
    Closes #714.
    
    Signed-off-by: Alex Dunn <dunn.alex@gmail.com>

--- screen.rb
+++ screen.rb
@@ -3,9 +3,9 @@ class Screen < Formula
   homepage "https://www.gnu.org/software/screen"
 
   stable do
-    url "https://ftpmirror.gnu.org/screen/screen-4.4.0.tar.gz"
-    mirror "https://ftp.gnu.org/gnu/screen/screen-4.4.0.tar.gz"
-    sha256 "ef722a54759a3bf23aad272bbf33c414c1078cad6bcd982fada93c0d7917218b"
+    url "https://ftpmirror.gnu.org/screen/screen-4.5.0.tar.gz"
+    mirror "https://ftp.gnu.org/gnu/screen/screen-4.5.0.tar.gz"
+    sha256 "01c3a7c362185f35d6a95dff52d64337076496acd034d717de3c263500cfefb0"
 
     # This patch is to disable the error message
     # "/var/run/utmp: No such file or directory" on launch
