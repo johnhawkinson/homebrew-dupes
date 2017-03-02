cc2628b6142f913e92a9159e3ded4c32fb65cd4f
Miguel Araújo <miguelarauj1o@gmail.com>
2017-03-02 20:57:35 -0300

    srm: update homepage
    Closes #732.
    
    Signed-off-by: Baptiste Fontaine <b@ptistefontaine.fr>

--- srm.rb
+++ srm.rb
@@ -1,6 +1,6 @@
 class Srm < Formula
   desc "command-line program to delete files securely"
-  homepage "http://srm.sourceforge.net/"
+  homepage "https://srm.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/srm/1.2.15/srm-1.2.15.tar.gz"
   sha256 "7583c1120e911e292f22b4a1d949b32c23518038afd966d527dae87c61565283"
 
