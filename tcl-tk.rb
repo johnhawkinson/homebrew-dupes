430487e1ec58011b404d2623699792efd9e23840
FX Coudert <fxcoudert@gmail.com>
2017-03-16 19:16:24 +0100

    tcl-tk: add mirrors
    Closes #734.
    
    Signed-off-by: FX Coudert <fxcoudert@gmail.com>

--- tcl-tk.rb
+++ tcl-tk.rb
@@ -2,6 +2,7 @@ class TclTk < Formula
   desc "Tool Command Language"
   homepage "https://www.tcl.tk/"
   url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.6/tcl8.6.6-src.tar.gz"
+  mirror "ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tcl8.6.6-src.tar.gz"
   version "8.6.6"
   sha256 "a265409781e4b3edcc4ef822533071b34c3dc6790b893963809b9fe221befe07"
 
@@ -25,6 +26,7 @@ class TclTk < Formula
 
   resource "tk" do
     url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.6/tk8.6.6-src.tar.gz"
+    mirror "ftp://ftp.tcl.tk/pub/tcl/tcl8_6/tk8.6.6-src.tar.gz"
     version "8.6.6"
     sha256 "d62c371a71b4744ed830e3c21d27968c31dba74dd2c45f36b9b071e6d88eb19d"
   end
