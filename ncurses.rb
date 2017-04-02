a5cd247a0d7b1f0ec00423a7a86113948dccf4e4
BrewTestBot <brew-test-bot@googlegroups.com>
2017-04-02 02:57:28 +0100

    ncurses: update 6.0_3 bottle.

--- ncurses.rb
+++ ncurses.rb
@@ -7,9 +7,9 @@ class Ncurses < Formula
   revision 3
 
   bottle do
-    sha256 "03544fbc2da911adbd75ef5c15e7de3fad5860734edb7163f612d8e8ee2d2435" => :el_capitan
-    sha256 "2ee55629127590c4e676649eb7e1bdc76f596fb69a632b0afde2e4f34ec14b1d" => :yosemite
-    sha256 "ebab4ef644c3ef62e6406a636baca078a3c09254671d7cd8ef48db05bd7b11d0" => :mavericks
+    sha256 "0b9103ce95f809c4ac104679a0c44f54bc2374c517447da9eb55b85bcad7d675" => :sierra
+    sha256 "daf1454abfe7785a642cc614e1b671f1e95ea797d9d1d0daaa5e7cb3800e0b69" => :el_capitan
+    sha256 "a73f869e5dc82d43fa05cdb86200a8a7b7e04b4da1902e9cdf77c90a94d678ad" => :yosemite
   end
 
   keg_only :provided_by_osx
